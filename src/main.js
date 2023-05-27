35097e4020d12b935fcca94d918b585aff5ee68f
KingZ060 <103080501+KingZ060@users.noreply.github.com>
2023-05-27 00:28:06 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 
 let config={
-    type:Phaser.WEBGL,
-    width: 960,
-    heigh: 540,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+        width: 960,
+        heigh: 540,
+    },
+    type:Phaser.AUTO,
     physics: {
         default: 'arcade',
         arcade: {
             debug: false,
             //debug: true,
         }
-    },
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
     },
     scene: [Intro,scene1,scene2,scene3,Ending]
 };
